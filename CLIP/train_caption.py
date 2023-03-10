f6945382aeeb981e543115aa95ee10f68926f966
zhuluntsai <william041107@gmail.com>
2023-03-10 15:26:28 +0800

    clip caption update

--- CLIP/train_caption.py
+++ CLIP/train_caption.py
@@ -42,8 +42,9 @@ class ClipPairDataset(Dataset):
         annotation = self.pair_list[item]    
         image_path = os.path.join(self.image_path, annotation['file_name'])
 
-        image =  torch.tensor(self.preprocess(Image.open(image_path)))
-        text = clip.tokenize(annotation[self.key])
+        image =  self.preprocess(Image.open(image_path))
+        text = clip.tokenize(annotation[self.key])[0]
+
         return image, text
 
     def __init__(self, preprocess, json_path: str, image_path: str, train_ratio: str, key: str, split: str):
@@ -56,12 +57,9 @@ class ClipPairDataset(Dataset):
         annotations = [annotation for annotation in annotations if annotation[key] != '']
 
         train_amount = int(len(annotations) * train_ratio)
-        pair_list = {'train': annotations[:train_amount], 'test': annotations[:train_amount]}
+        pair_list = {'train': annotations[:train_amount], 'test': annotations[train_amount:]}
 
         self.pair_list = pair_list[split]
-        print(self.pair_list[0])
-        print(len(self.pair_list))
-
         
 def main():
     print("Torch version:", torch.__version__)
@@ -82,7 +80,7 @@ def main():
     test_step = 0
     combination_num = 9
     key = 'violation_list' # caption_type violation_type
-    batch_size = 1
+    batch_size = 8
     epochs = 1000
     output_dir = 'models'
     output_prefix = 'clip'
@@ -91,7 +89,7 @@ def main():
     image_path = '../'
     lr = 1e-5
     num_warmup_steps = 5000
-    save_every = 100
+    save_every = 500
     train_ratio = 0.8
 
     model_name = f'{model_prefix}_comb{combination_num}'
@@ -121,10 +119,10 @@ def main():
         model.train()
         for idx, (image, text) in enumerate(train_dataloader):
             model.zero_grad()
-            image, text = image.to(device).squeeze(0), text.to(device).squeeze(0)
+            image, text = image.to(device), text.to(device)
             
             logits_per_image, logits_per_text = model(image, text)
-            label = torch.arange(logits_per_image.shape[0]).to(device)
+            label = torch.arange(len(image)).to(device)
 
             loss_i = criterion(logits_per_image, label)
             loss_t = criterion(logits_per_text, label)
@@ -156,7 +154,7 @@ def main():
         model.eval()
         with torch.no_grad():
             for idx, (image, text) in enumerate(test_dataloader):
-                image, text = image.to(device).squeeze(0), text.to(device).squeeze(0)
+                image, text = image.to(device), text.to(device)
                 
                 logits_per_image, logits_per_text = model(image, text)
                 label = torch.arange(logits_per_image.shape[0]).to(device)
